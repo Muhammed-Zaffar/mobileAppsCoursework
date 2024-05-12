b9c6be00872bd6d2f795ba9f8ef5a8b6961237e2
Muhammed-Zaffar <m.kaduji-22@student.lboro.ac.uk>
2024-05-12 18:01:15 +0100

    updated calulator screen

--- app/src/main/java/com/example/myapplication/CalculatorScreen.kt
+++ app/src/main/java/com/example/myapplication/CalculatorScreen.kt
@@ -1,22 +1,22 @@
 package com.example.myapplication
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,42 +56,78 @@ fun CalculatorScreen() {
                 verticalArrangement = Arrangement.SpaceBetween,
                 horizontalAlignment = Alignment.Start
             ){
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                    label = {
-                        Text(
-                            text = "Distance",
-                            style = MaterialTheme.typography.bodyMedium,
-                            fontFamily = FontFamily(Font(R.font.jetbrainsmono_variablefont_wght))
-                        )
-                    },
-                    modifier = Modifier.padding(start = 10.dp)
-                )
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                    label = {
-                        Text(
-                            text = "Fuel Price",
-                            style = MaterialTheme.typography.bodyMedium,
-                            fontFamily = FontFamily(Font(R.font.jetbrainsmono_variablefont_wght))
-                        )
-                    },
-                    modifier = Modifier.padding(start = 10.dp)
-                )
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                    label = {
-                        Text(
-                            text = "Fuel Consumption",
-                            style = MaterialTheme.typography.bodyMedium,
-                            fontFamily = FontFamily(Font(R.font.jetbrainsmono_variablefont_wght))
-                        )
-                    },
-                    modifier = Modifier.padding(start = 10.dp)
-                )
+                Row {
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        label = {
+                            Text(
+                                text = "Distance",
+                                style = MaterialTheme.typography.bodyMedium,
+                                fontFamily = FontFamily(Font(R.font.jetbrainsmono_variablefont_wght)),
+                                modifier = Modifier.padding(bottom = 20.dp)
+                            )
+                        },
+                        modifier = Modifier
+                            .padding(start = 10.dp, bottom = 10.dp)
+                            .fillMaxWidth(0.75f),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+                    Text(
+                        text = "miles",
+                        modifier = Modifier.padding(start = 10.dp, top = 20.dp),
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontFamily = FontFamily(Font(R.font.jetbrainsmono_variablefont_wght))
+                    )
+                }
+                Row {
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        label = {
+                            Text(
+                                text = "Fuel Price",
+                                style = MaterialTheme.typography.bodyMedium,
+                                fontFamily = FontFamily(Font(R.font.jetbrainsmono_variablefont_wght)),
+                                modifier = Modifier.padding(bottom = 20.dp)
+                            )
+                        },
+                        modifier = Modifier
+                            .padding(start = 10.dp, bottom = 10.dp)
+                            .fillMaxWidth(0.75f),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+                    Text(
+                        text = "GBP",
+                        modifier = Modifier.padding(start = 10.dp, top = 20.dp),
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontFamily = FontFamily(Font(R.font.jetbrainsmono_variablefont_wght))
+                    )
+                }
+                Row {
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        label = {
+                            Text(
+                                text = "Fuel Consumption",
+                                style = MaterialTheme.typography.bodyMedium,
+                                fontFamily = FontFamily(Font(R.font.jetbrainsmono_variablefont_wght)),
+                                modifier = Modifier.padding(bottom = 20.dp),
+                            )
+                        },
+                        modifier = Modifier
+                            .padding(start = 10.dp, bottom = 10.dp)
+                            .fillMaxWidth(0.75f),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    )
+                    Text(
+                        text = "mpg",
+                        modifier = Modifier.padding(start = 10.dp, top = 20.dp),
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontFamily = FontFamily(Font(R.font.jetbrainsmono_variablefont_wght))
+                    )
+                }
             }
         }
 
