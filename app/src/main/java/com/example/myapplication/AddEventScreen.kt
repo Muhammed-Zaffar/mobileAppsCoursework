2224b3ba652251a32daa0a58c26f494766f060f0
Muhammed-Zaffar <m.kaduji-22@student.lboro.ac.uk>
2024-05-14 20:42:02 +0100

    refactoring

--- app/src/main/java/com/example/myapplication/AddEventScreen.kt
+++ app/src/main/java/com/example/myapplication/AddEventScreen.kt
@@ -17,12 +17,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -32,9 +29,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -43,7 +37,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.util.Calendar
-import kotlin.math.log
 
 fun checkIfAnyFieldIsEmpty(
     date: String, mileage: String, fuelStation: String,
@@ -286,13 +279,7 @@ fun AddEventScreen() {
             onClick = {
                 val result =
                     if (!checkIfAnyFieldIsEmpty(
-                            date.value,
-                            mileage,
-                            fuelStation,
-                            fuelType,
-                            litres,
-                            price,
-                            totalCost
+                            date.value, mileage, fuelStation, fuelType, litres, price, totalCost
                         )
                     ) {
                         // add the event to the list then sync with database
