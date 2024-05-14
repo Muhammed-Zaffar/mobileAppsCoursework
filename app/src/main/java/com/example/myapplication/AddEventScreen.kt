39deaccc0f7c69c0e4795cf8c96066e5f02aff2f
Muhammed-Zaffar <m.kaduji-22@student.lboro.ac.uk>
2024-05-14 20:39:44 +0100

    gave up on the date input box, jsut leaving it as a typeable field

--- app/src/main/java/com/example/myapplication/AddEventScreen.kt
+++ app/src/main/java/com/example/myapplication/AddEventScreen.kt
@@ -1,13 +1,10 @@
 package com.example.myapplication
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import android.app.DatePickerDialog
 import android.content.Context
 import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,10 +15,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -30,6 +31,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -38,6 +43,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.util.Calendar
+import kotlin.math.log
 
 fun checkIfAnyFieldIsEmpty(
     date: String, mileage: String, fuelStation: String,
@@ -70,10 +76,12 @@ fun ShowDatePicker(
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddEventScreen() {
     val context = LocalContext.current
     var answer by remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() } // Create a focus requester
     // Create a new FuellingEvent object
     var date: MutableState<String> = remember { mutableStateOf("") }
     var mileage by remember { mutableStateOf("") }
@@ -122,8 +130,10 @@ fun AddEventScreen() {
             ) {
                 OutlinedTextField(
                     value = date.value,
-                    onValueChange = { date.value = it },
-                    readOnly = true,
+                    onValueChange = { },
+//                    readOnly = true,
+//                    enabled = false,
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Ascii),
                     label = {
                         Text(
                             text = "Date",
@@ -133,23 +143,16 @@ fun AddEventScreen() {
                     },
                     trailingIcon = {
                         Icon(Icons.Filled.DateRange, contentDescription = "Select Date",
-                            modifier = Modifier.clickable {
-                                ShowDatePicker(
-                                    context,
-                                    date
-                                )
-                            })
+                            modifier = Modifier
+                                .clickable {
+                                    ShowDatePicker(context, date)
+                                    Log.d("AddEventScreen","Date: ${date.value}") // Log the selected date
+                                }
+                        )
                     },
                     modifier = Modifier
                         .padding(start = 10.dp, bottom = 10.dp)
                         .fillMaxWidth(0.75f)
-                        .clickable {
-                            ShowDatePicker(
-                                context,
-                                date
-                            )
-                            Log.d("AddEventScreen", "Date: ${date.value}") // Log the selected date
-                        },  // Open DatePicker when the TextField is clicked
                 )
 
                 Row {
